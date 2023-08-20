598a033484ac675d08565b88c2fae7a08942053a
risthian-P <cristhian.panora@gmail.com>
2023-08-19 21:09:53 -0500

    1. boton limpiar datos _ falta setear signos, mejor encontramos get 2. falta conexion de base de datos

--- src/formulario.java
+++ src/formulario.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class formulario {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class formulario {
+    private JPanel rootPanel;
+    private JTextField textCodigo;
+    private JComboBox SignoComboBox;
+    private JButton buscarPorCódigoButton;
+    private JButton buscarPorNombreButton;
+    private JButton buscarPorSignoButton;
+    private JButton limpiarFormularioButton;
+    private JButton borrarElPresenteRegistroButton;
+    private JButton actualizarElPresenteRegistroButton;
+    private JButton ingresarElPresenteRegistroButton;
+    private JTextField textCedula;
+    private JTextField textNombre;
+    private JTextField textFecha;
+
+    private formulario(){
+        limpiarFormularioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textCodigo.setText("");
+                textCedula.setText("");
+                textNombre.setText("");
+                textFecha.setText("");
+                //falta setear el valor del signo
+                System.out.println(SignoComboBox.getSelectedIndex());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("formulario");
+        frame.setContentPane(new formulario().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
