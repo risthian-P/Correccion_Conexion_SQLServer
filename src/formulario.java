0483f73b8c598bf928dffadb408b070d4d0c54d2
risthian-P <cristhian.panora@gmail.com>
2023-08-23 22:49:18 -0500

    1. boton limpiar datos _ falta setear signos, mejor encontramos get 2. falta conexion de base de datos

--- src/formulario.java
+++ src/formulario.java
@@ -32,27 +32,28 @@ public class formulario {
             }
         });
     }
-    public void establecerConexion(){
-        String DB_url = "jdbc:sqlserver://localhost:1433;databaseName=FORMULARIO";
-        String usuario = "sa";
-        String contrasena = "sa";
+    private Connection establecerConexion() {
+        String DB_url = "jdbc:sqlserver://localhost:1433;databaseName=FORMULARIO;encrypt=true;trustServerCertificate=true";
+        String usuario = "java_conn";
+        String contrasena = "java_conn";
         String query = "Select * from registro";
+        Connection conectar;
         try {
-            Connection conectar = DriverManager.getConnection(DB_url,usuario,contrasena);
+            conectar = DriverManager.getConnection(DB_url, usuario, contrasena);
             Statement stmt = conectar.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             System.out.println("conexion exitosa");
-            while(rs.next()){
-                System.out.println("\nid: "+rs.getString("id"));
-                System.out.println("nombre: "+rs.getString("nombre"));
-                System.out.println("apellido: "+rs.getString("apellido"));
-                System.out.println("edad: "+rs.getBigDecimal("edad"));
-                System.out.println("profesion: "+rs.getString("profesion"));
+            while (rs.next()) {
+                System.out.println("\nid: " + rs.getString("id"));
+                System.out.println("nombre: " + rs.getString("nombre"));
+                System.out.println("apellido: " + rs.getString("apellido"));
+                System.out.println("edad: " + rs.getBigDecimal("edad"));
+                System.out.println("profesion: " + rs.getString("profesion"));
             }
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return conectar;
     }
     public static void main(String[] args) {
         JFrame frame = new JFrame("formulario");
